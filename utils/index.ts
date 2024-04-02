66387855178592596f287f76a17af451d24308a4
Mike <marko.v.polo.solo@gmail.com>
2024-04-02 21:34:29 +0300

    create dashboard view

--- utils/index.ts
+++ utils/index.ts
@@ -58,3 +58,62 @@ export const dashboardLinks = [
     },
     
 ]
+export const tools = [
+
+    {
+
+        label: 'Dashboard',
+        icon:'lucide:layout-dashboard',
+        link: '/dashboard',
+        color: 'text-sky-500',
+        bgColor: 'bg-sky-500/10',
+
+    },
+    {
+
+        label: 'Conversation',
+        icon:'lucide:message-circle',
+        link: '/conversation',
+        color: 'text-violet-500',
+        bgColor: 'bg-violet-500/10',
+
+    },
+    {
+
+        label: 'image',
+        icon:'lucide:image',
+        link: '/image',
+        color: 'text-pink-700',
+        bgColor: 'bg-pink-700/10',
+
+    },
+    {
+
+        label: 'video',
+        icon:'lucide:video',
+        link: '/video',
+        color: 'text-orange-700',
+        bgColor: 'bg-orange-700/10',
+
+    },
+    {
+
+        label: 'music',
+        icon:'lucide:music',
+        link: '/music',
+        color: 'text-emerald-700',
+        bgColor: 'bg-emerald-700/10',
+
+    },
+    {
+
+        label: 'code',
+        icon:'lucide:code',
+        link: '/code',
+        color: 'text-blue-700',
+        bgColor: 'bg-blue-700/10',
+
+    },
+
+    
+]
